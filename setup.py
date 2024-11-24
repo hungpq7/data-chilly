478007eae07a695e731c7e0c20cf8d1ad27e5481
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2024-11-24 23:30:08 +0700

    -

---
+++ setup.py
@@ -0,0 +1,16 @@
+from urllib.request import urlretrieve
+from IPython.display import clear_output
+
+DBFILE = 'chinook.db'
+
+urlretrieve(f'https://github.com/hungpq7/data-chilly/blob/sql/data/{DBFILE}', DBFILE)
+ipython = get_ipython()
+ipython.run_line_magic("pip", "install ipython-sql==0.5.0")
+ipython.run_line_magic("reload_ext", "sql")
+ipython.run_line_magic("sql", f"sqlite:///{DBFILE}")
+ipython.run_line_magic("config", "SqlMagic.style='_DEPRECATED_DEFAULT'")
+ipython.run_line_magic("config", "SqlMagic.displaycon=False")
+ipython.run_line_magic("config", "SqlMagic.feedback=False")
+
+clear_output()
+print(f'{DBFILE} connected successfully!')
