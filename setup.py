6d5a8f7a5b11a0289af923eb51f3b74f4afcc62a
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2024-11-25 00:05:16 +0700

    -

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
+import sys
 from urllib.request import urlretrieve
 from IPython.display import clear_output
 
 DBFILE = 'chinook.db'
 
 def setup():
-    urlretrieve(f'https://github.com/hungpq7/data-chilly/blob/sql/data/{DBFILE}', DBFILE)
+    dbfile = sys.argv[1]
+    urlretrieve(f'https://github.com/hungpq7/data-chilly/blob/sql/data/{dbfile}', dbfile)
     ipython = get_ipython()
     ipython.run_line_magic("pip", "install ipython-sql==0.5.0")
     ipython.run_line_magic("reload_ext", "sql")
-    ipython.run_line_magic("sql", f"sqlite:///{DBFILE}")
+    ipython.run_line_magic("sql", f"sqlite:///{dbfile}")
     ipython.run_line_magic("config", "SqlMagic.style='_DEPRECATED_DEFAULT'")
     ipython.run_line_magic("config", "SqlMagic.displaycon=False")
     ipython.run_line_magic("config", "SqlMagic.feedback=False")
 
     clear_output()
-    print(f'{DBFILE} connected successfully!')
+    print(f'{dbfile} connected successfully!')
 
 setup()
