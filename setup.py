9a23a0c68e064cf10a9b7fa37890cbc638887e67
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2024-11-25 07:22:38 +0700

    -

--- setup.py
+++ setup.py
@@ -4,18 +4,14 @@ from IPython.display import clear_output
 
 DBFILE = 'chinook.db'
 
-def setup():
-    dbfile = sys.argv[1]
-    urlretrieve(f'https://github.com/hungpq7/data-chilly/raw/sql/data/{dbfile}', dbfile)
-    ipython = get_ipython()
-    ipython.run_line_magic("pip", "install ipython-sql==0.5.0")
-    ipython.run_line_magic("reload_ext", "sql")
-    ipython.run_line_magic("sql", f"sqlite:///{dbfile}")
-    ipython.run_line_magic("config", "SqlMagic.style='_DEPRECATED_DEFAULT'")
-    ipython.run_line_magic("config", "SqlMagic.displaycon=False")
-    ipython.run_line_magic("config", "SqlMagic.feedback=False")
+dbfile = sys.argv[1]
+urlretrieve(f'https://github.com/hungpq7/data-chilly/raw/sql/data/{dbfile}', dbfile)
+get_ipython().run_line_magic("pip", "install ipython-sql==0.5.0")
+get_ipython().run_line_magic("reload_ext", "sql")
+get_ipython().run_line_magic("sql", f"sqlite:///{dbfile}")
+get_ipython().run_line_magic("config", "SqlMagic.style='_DEPRECATED_DEFAULT'")
+get_ipython().run_line_magic("config", "SqlMagic.displaycon=False")
+get_ipython().run_line_magic("config", "SqlMagic.feedback=False")
 
-    clear_output()
-    print(f'{dbfile} connected successfully!')
-
-setup()
+clear_output()
+print(f'{dbfile} connected successfully!')
