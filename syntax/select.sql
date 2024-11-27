bf04adcdbcf46a0d50e9dc447fb354c8a42285a4
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2024-11-27 21:14:13 +0700

    -

---
+++ syntax/select.sql
@@ -0,0 +1,14 @@
+-- select all columns
+SELECT *
+FROM my_table;
+
+-- select specific columns
+SELECT column_1, column_2,..., column_n
+FROM my_table
+
+-- select specific columns with aliases
+SELECT column_1 AS alias_1
+    , column_2 AS alias_2
+    , ...
+    , column_n AS alias_n
+FROM my_table
