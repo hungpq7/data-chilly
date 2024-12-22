7fde4d10921291f85d6e199bbd7c0cf98bc0207d
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2024-12-22 07:43:06 +0700

    -

--- syntax/if_else.py
+++ syntax/if_else.py
@@ -1,13 +1,13 @@
 if condition_1:
-    # code to be executed
+    # code block to be executed
     # if the first condition is true
 elif condition_2:
-    # code to be executed
+    # code block to be executed
     # if all previous conditions are false
     # and current condition is true
 ...
 elif condition_n:
     # body
 else:
-    # code to be executed
+    # code block to be executed
     # if all conditions are false
