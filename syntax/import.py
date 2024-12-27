167b0ba9e0307b7925567ad962b29d6e656d311d
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2024-12-27 21:29:26 +0700

    -

---
+++ syntax/import.py
@@ -0,0 +1,11 @@
+# basic import
+import numpy
+
+# import with an alias
+import numpy as np
+
+# import specific functions/objects
+from numpy import pi, sqrt
+
+# import a sub-module
+from scipy.stats as stats
